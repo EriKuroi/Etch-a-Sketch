8e32de20c13a3ed20a31a2908d7ee28557725b58
Eri <eryka.sh@gmail.com>
2018-06-21 18:00:05 +0300

    Add pad generation and rainbow mode

--- script.js
+++ script.js
@@ -1,30 +1,78 @@
 window.addEventListener('DOMContentLoaded', function () {
-
-    for (let i = 0; i < 256; i++) {
-
-        let div = document.createElement('div');
-        div.className = 'cell'
-        div.id = `${i}`
-        document.getElementById('board').appendChild(div);     // Append <li> to <ul> with id="myList"
-    }
+    const fragment = document.createDocumentFragment();
     const board = document.getElementById('board');
-    function paint(cell) {
-        cell.classList.add('color');
+    const clear = document.getElementById('clear');
+    const newPad = document.getElementById('newPad');
+    const newMode = document.getElementById('newMode');
+    //const allCells = document.querySelectorAll('.cell');
+    let state = 'black';
+    createPad(40);
+
+    function createPad(cellsRowQuantity) {
+        setSide = Number(cellsRowQuantity);
+        if (setSide == 0 || isNaN(setSide)) {
+            return console.log('wrong');
+        } else {
+            console.log(setSide);
+            Array.from(board.children).forEach(function (e) {
+                e.remove();
+            });
+            const side = setSide;
+            const allCells = Math.pow(side, 2);
+            const cellSide = 80 / side;
+            for (let i = 0; i < allCells; i++) {
+
+                let div = document.createElement('div');
+                div.style.width = `${cellSide}vw`;
+                div.style.height = `${cellSide}vw`;
+                div.className = 'cell';
+                div.id = `${i}`;
+                fragment.appendChild(div);
+            }
+            document.getElementById('board').appendChild(fragment);
+        }
     }
 
 
-    board.addEventListener('mouseover', function(e) {
-        if(/cell/.test(e.target.className)) {
+    function paint(cell) {
+        const randomHsl = () => `hsla(${Math.random() * 360}, 100%, 50%, 1)`;
+        const color = {
+            blackState: 'black',
+            rainbowState: randomHsl()
+        }
+        state === 'black' ? cell.style.backgroundColor = color.blackState : cell.style.backgroundColor = color.rainbowState;
+
+    }
+
+    function changeMode() {
+        if (state === 'black') {
+            state = 'rainbow';
+            newMode.innerHTML = 'black mode';
+        } else {
+            state = 'black';
+            newMode.innerHTML = 'rainbow mode';
+
+        }
+    }
+
+    board.addEventListener('mouseover', function (e) {
+        if (/cell/.test(e.target.className)) {
             let cell = e.target;
             paint(cell);
         }
     }, false);
-    const clear = document.getElementById('clear');
     clear.addEventListener('click', function () {
         Array.from(board.children).forEach(function (e) {
             e.classList.remove('color');
+            e.style.backgroundColor = 'white';
         })
-        window.prompt('Insert quantity of cells in a row: 16 or 64')
+    })
+    newPad.addEventListener('click', function () {
+        const newPadSide = window.prompt('Insert quantity of cells in a row', '40');
+        createPad(newPadSide);
+    })
+    newMode.addEventListener('click', function () {
+        changeMode();
     })
 
 
